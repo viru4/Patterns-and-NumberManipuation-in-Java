7e39cdbe3244dd85e259efdc238722d0204e53e0
Virendra Kumar <vkprajapativiru.com>
2025-07-31 12:03:43 +0530

    Add Divisor.java to NumberManipulation folder

--- NumberManipulation/Divisor.java
+++ NumberManipulation/Divisor.java
@@ -7,7 +7,7 @@ public class Divisor {
         //int n=sc.nextInt();
         int n=8;
         for (int i = 1; i <=n; i++) {
-            if((n/i)==0){
+            if((n%i)==0){
                 System.out.println(i);
             }
         }

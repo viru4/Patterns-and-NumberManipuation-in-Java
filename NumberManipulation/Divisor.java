1b5b987017bd5fca22fa5dcc30dfab84ef25f932
Virendra Kumar <vkprajapativiru.com>
2025-07-31 11:59:22 +0530

    new file Divisor.java

---
+++ NumberManipulation/Divisor.java
@@ -0,0 +1,15 @@
+
+
+public class Divisor {
+    public static void main(String[] args) {
+        //Scanner sc=new Scanner(System.in);
+        //System.out.println("enter the number");
+        //int n=sc.nextInt();
+        int n=8;
+        for (int i = 1; i <=n; i++) {
+            if((n/i)==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
